96977841cb234ab97d374c6ffdfdae1c1fa36a6f
Tarcisio Nunes <github@tarcnux.com.br>
2021-09-23 23:03:30 -0300

    Work done

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 // Add imports here
-
+const BIP39 = require("bip39")
+const hdkey = require("ethereumjs-wallet/hdkey")
+const Wallet = require('ethereumjs-wallet')
+const keccak256 = require('js-sha3').keccak256
+const EthereumTx = require('ethereumjs-tx')
 
 
 // Add functions here
+// Generate a random mnemonic (uses crypto.randomBytes under the hood), defaults to 128-bits of entropy
+function generateMnemonic() {
+    return BIP39.generateMnemonic()
+}
 
+var isValid = BIP39.validateMnemonic("Enter your mnemonic here")
+// This will return false because "Enter your mneomnic here" is not a valid phrase
 
+function generateSeed(mnemonic) {
+    return BIP39.mnemonicToSeed(mnemonic)
+}
+
+function generatePrivKey(mnemonic) {
+    const seed = generateSeed(mnemonic)
+    return hdkey.fromMasterSeed(seed).derivePath(`m/44'/60'/0'/0/0`).getWallet().getPrivateKey()
+}
+
+function derivePubKey(privkey) {
+    const wallet = Wallet.fromPrivateKey(privkey)
+    return wallet.getPublicKey()
+}
+
+function deriveEthAddress(pubKey) {
+    const address = keccak256(pubKey) // keccak256 hash of  publicKey
+     // Get the last 20 bytes of the public key
+     return "0x" + address.substring(address.length - 40, address.length)
+}
+
+function signTx(privKey, txData) {
+    const tx = new EthereumTx(txData)
+    tx.sign(privKey)
+    return tx
+}
+
+function getSignerAddress(signedTx) {
+    return "0x" + signedTx.getSenderAddress().toString('hex')
+}
 
 /*
 
@@ -32,7 +71,7 @@ var mnemonicVue = new Vue({
         recoveredAddress: ""
     },
     methods:{
-        generateNew: function(){
+        generateNew: function(){            
             this.mnemonic = generateMnemonic()
         },
         signSampleTx: function(){
